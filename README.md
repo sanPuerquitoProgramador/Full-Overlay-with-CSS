8992c6982b4330832a54e6bea2a0073485c8b4de
Eduardo Cabrera <ivan.cabrera@havas-se.com>
2017-01-11 15:31:03 -0600

    commit initia, everything is working

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # overlay
+Simple overlay with CSS and without dependencies. Super usesful for display simples messages E.g. When the site is temporaly offline
---
+++ p.js
@@ -0,0 +1,6 @@
+function openOverlay() {
+	document.getElementById("myOverlay").style.width = "100%";
+}
+function closeOverlay() {
+	document.getElementById("myOverlay").style.width = "0%";
+}
